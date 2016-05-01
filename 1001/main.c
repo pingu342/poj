3a416354e6446a6ba45e4c65ab75bc4a5ed70970
pingu342 <pingu342@users.noreply.github.com>
2016-05-01 21:06:04 +0900

    Modify 1001

--- 1001/main.c
+++ 1001/main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX (100)
+#define MAX (1000)
 #define ASSERT(x) { if (!(x)) { printf("%d: assert error.\n", __LINE__); abort(); } }
 
 typedef struct {
 	long mantissa[MAX]; //mantissa[0]:仮数部の1の位, mantissa[1]:仮数部の2の位, ...
-	long exponent;
+	long exponent;      //指数部
+	long msd;           //仮数部最上位桁(most significant digit)
 } BigNum;
 
 static void parse(BigNum *bigNum, char *buf)
@@ -15,6 +16,8 @@ static void parse(BigNum *bigNum, char *buf)
 	int i=0, n;
 	long leadingZero=1;
 	char *p = strchr(buf, '\0');
+	ASSERT(p!=NULL);
+	ASSERT((p-buf)<MAX);
 	p--;
 	bigNum->exponent = 0;
 	while (i<MAX) {
@@ -25,6 +28,7 @@ static void parse(BigNum *bigNum, char *buf)
 			//skip
 		} else {
 			n = *p - 0x30;
+			bigNum->msd = i;
 			bigNum->mantissa[i++] = n;
 			bigNum->exponent--;
 			leadingZero = 0;
@@ -33,6 +37,7 @@ static void parse(BigNum *bigNum, char *buf)
 	}
 	while (i<MAX) {
 		n = *p - 0x30;
+		bigNum->msd = i;
 		bigNum->mantissa[i++] = n;
 		if (p == buf) {
 			break;
@@ -44,15 +49,18 @@ static void parse(BigNum *bigNum, char *buf)
 	}
 }
 
+// a = a * b
 static void mul(BigNum *a, BigNum *b)
 {
+	// アルゴリズムは筆算と同じ
 	BigNum origin_a;
 	long tmp;
 	int i, j;
 	memcpy(&origin_a, a, sizeof(BigNum));
 	parse(a, "0.0");
-	for (i=0; i<MAX; i++) {
-		for (j=0; j<MAX; j++) {
+	for (i=0; i<=b->msd; i++) {
+		for (j=0; j<=origin_a.msd; j++) {
+			ASSERT((i+j+1)<MAX);
 			tmp = a->mantissa[i+j];
 			tmp += b->mantissa[i] * origin_a.mantissa[j];
 			a->mantissa[i+j] = (tmp % 10);
@@ -60,13 +68,14 @@ static void mul(BigNum *a, BigNum *b)
 		}
 	}
 	a->exponent = origin_a.exponent + b->exponent;
+	a->msd = b->msd + origin_a.msd + 1;
 }
 
 static char *toString(char *buf, BigNum *bigNum)
 {
 	char *p = buf;
 	int i, j, n, exponent;
-	for (i=MAX-1; i>=0; i--) {
+	for (i=bigNum->msd; i>=0; i--) {
 		if (bigNum->mantissa[i] != 0) {
 			break;
 		}
