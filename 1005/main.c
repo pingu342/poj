356e610985450a7db83f218616063769f7819022
pingu342 <pingu342@users.noreply.github.com>
2016-05-03 19:58:03 +0900

    Add 1005

---
+++ 1005/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 土地は毎年50平方マイルだけ半円状に侵食される
+// そと半円の半径(r)と年数(i)の関係は、
+//    PI*r^2*0.5=50*i
+//    r^2=100/PI*i
+// 与えられた座標(x,y)と半円の半径(r)の関係は、
+//    r^2=x^2+y^2
+// 座標(x,y)が侵食されるまでの年数は、
+//    x^2+y^2=100/PI*i
+//    i=(x^2+y^2)/(100/PI)
+
+#define PI 3.141592
+
+int main()
+{
+	int i, n;
+	float x, y;
+	scanf("%d", &n);
+	for (i=1; i<=n && scanf("%f %f", &x, &y); i++) {
+		printf("Property %d: This property will begin eroding in year %d.\n",
+				i, (int)((x*x+y*y)/(100./PI)+1.));
+	}
+	printf("END OF OUTPUT.\n");
+}
