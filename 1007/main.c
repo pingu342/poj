c8cfcf2c1c24cd8f194e36a697d9be97a90dfbb7
pingu342 <pingu342@users.noreply.github.com>
2016-05-05 17:14:37 +0900

    Modify 1007

--- 1007/main.c
+++ 1007/main.c
@@ -1,5 +1,5 @@
-// A,C,G,Tの文字から成る文字列の配列を、文字列のsortednssが大きい順に並べ替える
-// 例えば文字列'DAABEC'のsortednssは、Dが右の4文字(A,A,B,C)より大きく、Eが右の1文字(C)より大きいので、それらを合計した5とする
+// A,C,G,Tの文字から成る文字列の配列を、文字列のunsortednessが大きい順に並べ替える
+// 例えば文字列'DAABEC'のunsortednessは、Dが右の4文字(A,A,B,C)より大きく、Eが右の1文字(C)より大きいので、それらを合計した5とする
 
 #include <stdio.h>
 #include <stdlib.h>
