6c8a8a79e19d9a72e39d7393c05026b22e6f6834
pingu342 <pingu342@users.noreply.github.com>
2016-05-15 22:55:40 +0900

    Modify 1010

--- 1010/main.c
+++ 1010/main.c
@@ -4,10 +4,12 @@
 //
 // プログラムへは1回につき2行ずつ入力され、1行目が現在の切手の在庫、2行目が顧客の希望を示す
 //
-// 1 2 3 0	; 現在の在庫：額面が1, 2, 3の切手がそれぞれ1種類ずつあり、在庫枚数に上限なし
+// 1 2 3 0	; 現在の在庫：額面が1, 2, 3の切手があり、在庫枚数に上限なし
 // 7 4 0	; 顧客の希望：顧客は2人で、希望する額面はそれぞれ7、4
 // 1 1 0	; 現在の在庫：額面が1の切手が2種類あり、在庫枚数に上限なし（額面は同じだが種類の異る切手が在庫にある）
 // 6 2 3 0	; 顧客の希望：顧客は3人で、希望する額面はそれぞれ6、2、3
+//
+// 切手は最大で25種類ある
 //
 // 下記の条件に従い、顧客に発行する切手を在庫から選んで出力する
 // ・顧客の希望する額面に正確に一致すること
@@ -55,8 +57,129 @@
 //    or
 //   $ g++ sample.cpp
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STAMP_TYPES_MAX (100)
+#define SELL_STAMPS_MAX (4)
+
+int Request;
+int SellStampTypesNum;
+int SellStampMax;
+int SellStampNum;
+int Sell[SELL_STAMPS_MAX+1];
+int Stock[STAMP_TYPES_MAX+1];
+int Temp[SELL_STAMPS_MAX+1];
+int Tie;
+
+void debug(const char *head, int *stampTypes, int size)
+{
+	int i;
+	printf("%s", head);
+	for (i=0; i<size; i++)
+		printf("%d ", stampTypes[i]);
+	printf("\n");
+}
+
+void init()
+{
+	int i;
+	SellStampTypesNum = SellStampMax = SellStampNum = 0;
+	for (i=0; i<SELL_STAMPS_MAX+1; i++) { Sell[i] = 0; Temp[i] = 0; }
+	Tie = 0;
+}
+
+void update(int typesNum, int stampNum, int maxStamp)
+{
+	SellStampTypesNum = typesNum;
+	SellStampMax = maxStamp;
+	SellStampNum = stampNum;
+	memcpy(Sell, Temp, sizeof(int)*stampNum);
+	Tie = 0;
+}
+
+void check()
+{
+	//debug("check: ", Temp, SELL_STAMPS_MAX+1); //debug
+	int i, *p, typesNum=1, maxStamp=Temp[0]%STAMP_TYPES_MAX;
+	for (i=1, p=&Temp[0]; Temp[i]; i++) {
+		if (*p != Temp[i]) {
+			typesNum++;
+			p = &Temp[i];
+		}
+		if (maxStamp < (Temp[i]%STAMP_TYPES_MAX))
+			maxStamp = Temp[i]%STAMP_TYPES_MAX;
+	}
+	if (SellStampTypesNum < typesNum)
+		update(typesNum, i, maxStamp);
+	else if (SellStampTypesNum == typesNum) {
+		if (SellStampNum > i)
+			update(typesNum, i, maxStamp);
+		else if (SellStampNum == i) {
+			if (SellStampMax < maxStamp)
+				update(typesNum, i, maxStamp);
+			else if (SellStampMax == maxStamp)
+				Tie = 1;
+		}
+	}
+}
+
+void search(int remain, int stockNo, int tempNo)
+{
+	//printf("search remain:%d stockNo:%d tempNo:%d\n", remain, stockNo, tempNo); //debug
+	//debug("   ", Temp, SELL_STAMPS_MAX+1);
+	int stamp = Stock[stockNo]%STAMP_TYPES_MAX, i;
+	if (tempNo<4 && Stock[stockNo+1])
+		search(remain, stockNo+1, tempNo);
+	for (i=1; i<=4-tempNo; i++) {
+		if (remain < stamp*i)
+			break;
+		Temp[tempNo+i-1] = Stock[stockNo];
+		if (remain == stamp*i) {
+			Temp[tempNo+i] = 0;
+			check();
+			break;
+		}
+		if (tempNo+i<4 && Stock[stockNo+1])
+			search(remain-stamp*i, stockNo+1, tempNo+i);
+	}
+}
+
+static int compare(const void *a, const void *b)
+{
+	return *(int *)a - *(int *)b;
+}
 
 int main()
 {
+	int i, *p;
+	while (scanf("%d", &Stock[0]) != EOF) {
+		for (i=0; Stock[i];)
+			scanf("%d", &Stock[++i]);
+		if (i==0)
+			break;
+		qsort(Stock, i, sizeof(int), compare);
+		for (i=1, p=&Stock[0]; Stock[i]; i++)
+			if (*p==Stock[i])
+				Stock[i] += (STAMP_TYPES_MAX * (&Stock[i] - p));
+			else
+				p = &Stock[i];
+		while (scanf("%d", &Request), Request) {
+			init();
+			search(Request, 0, 0);
+			if (SellStampNum) {
+				printf("%d (%d):", Request, SellStampTypesNum);
+				if (Tie)
+					printf(" tie");
+				else
+					for (i=0; i<SellStampNum; i++)
+						printf(" %d", Sell[i]%STAMP_TYPES_MAX);
+			} else {
+				printf("%d ---- none", Request);
+			}
+			printf("\n");
+		}
+	}
 }
 
